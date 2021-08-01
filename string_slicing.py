af604564cae7ab2e7c4ce4d450d4eaeb3ca707b9
atulpatel991928 <75467319+atulpatel991928@users.noreply.github.com>
2021-08-01 22:59:38 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,17 @@
+import string
+String1 = "Banaras"
+#print(String1)
+print(String1[3:12])
+print(String1[3:-2])
+print(String1[0:len(String1)])
+print(String1[0:1:2])
+print(String1)
+del String1[2]
+print(String1)
+
+'''String1 = "Hello, I'm a Geek: "
+String2 = "Hi i am steave"
+Newstring =String1 + String2
+print(Newstring)
+newString = String1.replace('Hello','')'''
+
